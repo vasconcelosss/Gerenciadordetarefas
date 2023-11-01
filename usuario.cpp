e899f5e1f2e17dbc41f7f5d5f6628a7fd5df4e1b
Isabela_Santos <99598839+isabela-santos-ufmg@users.noreply.github.com>
2023-10-31 23:34:04 -0300

    Update usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -7,6 +7,12 @@ Usuario::Usuario(std::string name){
     mkdir(nome_.c_str(), 0777);
 }
 
+// Retorno de membros privados da classe
 std::string Usuario::nomeUsuario(){
     return nome_;
+}
+
+// Métodos de manipulação da classe
+void Usuario::MudarNome(std::string novoNome){
+    nome_ = novoNome;
 }

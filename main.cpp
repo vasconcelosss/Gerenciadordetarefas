e3e6cd6e9f800cbca4cd86ec519a5548c0a1f845
Ana Clara Vasconcelos dos Santos <129204364+vasconcelosss@users.noreply.github.com>
2023-10-31 23:51:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,37 @@
 #include "projeto.h"
 #include "tarefa.h"
 
+void FuncoesTarefa(std::string usuario_atual, std::list<Tarefa>& tarefas, const std::string& tarefa_atual){
+
+    bool fim = false;
+    Tarefa* ptr_tarefa_atual = nullptr;
+        for(auto& taref : tarefas){
+            if (taref.NomeTarefa() == tarefa_atual) {
+                ptr_tarefa_atual = &taref;
+                break;
+            }
+        }
+    while(!fim){
+
+        std::cout << std::endl << "Usuário " << usuario_atual << std::endl;
+        std::cout << std::endl << "Caso você tenha interesse em: " << std::endl;
+        std::cout << "Renomear a Tarefa " << ptr_tarefa_atual->NomeTarefa() << ". Digite 1" << std::endl;
+        if(ptr_tarefa_atual->CaminhoDescricao() == ""){
+            std::cout << "Criar uma Descrição para a Tarefa. Digite 200" << std::endl;
+        } else{
+            std::cout << "Mudar a descrição (Apaga a anterior). Digite 201" << std::endl;
+            std::cout << "Exibir a descrição da Tarefa. Digite 202" << std::endl;
+        }
+        std::cout << "Concluir a Tarefa. Digite 3" << std::endl;
+        std::cout << "Ver o estatus de conclusão da Tarefa. Digite 4" << std::endl;
+        std::cout << "Ver a Data meta de Conclusão. Digite 5" << std::endl;
+        std::cout << "Ver a Prioridade da sua tarefa. Digite 6" << std::endl;        
+        std::cout << "Voltar as opções anteriores. Digite 7" << std::endl;
+
+       
+    }
+}
+
 void Inicio(std::vector<Usuario>& users){  
 bool fim= false;
     while(!fim){

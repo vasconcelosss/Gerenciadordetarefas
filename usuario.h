49495293740202e5b03470eba66b173a194754c2
Isabela_Santos <99598839+isabela-santos-ufmg@users.noreply.github.com>
2023-10-31 23:33:20 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,8 +1,11 @@
 #ifndef USUARIO_H_
 #define USUARIO_H_
 
+#include "projeto.h"
+
 #include <iostream>
 #include <string>
+#include <list>
 #include <fstream> // Para manipulação de arquivos
 #include <sys/stat.h>  
 #include <stdio.h>
@@ -12,14 +15,20 @@ class Usuario {
 
 public:
 
+    std::list<Projeto> Projetos;
+
     // Construtores:
     Usuario(std::string name);
 
     // Retorno de membros privados da classe
     std::string nomeUsuario();
 
+    // Métodos de manipulação da classe
+    void MudarNome(std::string novoNome); //Modifica o nome do Usuario para o novo passado
+
 private:
     std::string nome_;
+
 
 };
 

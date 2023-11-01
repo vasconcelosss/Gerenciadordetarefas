bded4c0290c403512a81cd9df116c6236e69e255
Isabela_Santos <99598839+isabela-santos-ufmg@users.noreply.github.com>
2023-10-31 23:37:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
 
+#include "usuario.h"
+#include "projeto.h"
+#include "tarefa.h"
+
+void Inicio(std::vector<Usuario>& users){  
+
+}
+
+int main(){
+
+    std::vector<Usuario> users;
+
+	std::cout<< "---- Bem vindo ao Gerenciador de Projetos! ----" << std::endl;
+	std::cout<< "Esta aplicação foi desenvolvida visando ser rodada em sistemas Unix!" << std::endl << std::endl;
+	
+    Inicio(users);
+
+    return 0;
+}

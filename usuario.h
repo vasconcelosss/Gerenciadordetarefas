8070fe90bb3bd44e72947a972598a04a951e0a1b
Isabela_Santos <99598839+isabela-santos-ufmg@users.noreply.github.com>
2023-12-05 22:02:39 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -9,7 +9,7 @@
 #include <fstream> // Para manipulação de arquivos
 #include <sys/stat.h>  
 #include <stdio.h>
-
+#include <algorithm>  // Para a função std::sort
 
 class Usuario {
 
@@ -21,14 +21,71 @@ public:
     Usuario(std::string name);
 
     // Retorno de membros privados da classe
-    std::string nomeUsuario();
+    std::string nomeUsuario() const;
 
     // Métodos de manipulação da classe
+    void AdicionaProjeto(Projeto p); //Adiciona um projeto a lista
+
     void MudarNome(std::string novoNome); //Modifica o nome do Usuario para o novo passado
 
-private:
-    std::string nome_;
+    void ListarProjetos(int forma) const; //Imprime os Nomes dos Projetos de acordo com o parametro passado
+    //Da Forma:
+    //1 - Por nome (Ordem Alfabetica do Nome)
+    //2 - Por Categoria (Ordem Alfabetica da Categoria)
+    //3 - Por Ordem de Criação
+    //Outro - Imprime "Forma não existente!"
 
+
+private:
+    
+    //Funções Privadas para uso em Métodos:
+
+    //1 - Por nome (Ordem Alfabetica do Nome)
+    // Recebe uma lista de Projetos
+    // Retorna uma lista dos nomes em ordem
+    std::list<std::string> OrdenaNomes1(const std::list<Projeto>& Projetos) const {
+        
+         std::list<std::string> nomesOrdenados;
+
+        for(const auto& projeto : Projetos){
+            nomesOrdenados.push_back(projeto.NomeProjeto());
+        }
+
+        // Ordenando a lista de nomes
+        nomesOrdenados.sort();
+
+        return nomesOrdenados;
+    }
+
+    //2 - Por Categoria (Ordem Alfabetica da Categoria)
+    // Recebe uma lista de Projetos
+    // Retorna uma lista dos Nomes ordenada
+    // de acordo com as categorias
+    std::list<std::string> OrdenaNomes2(const std::list<Projeto>& Projetos) const {
+
+        std::list<std::string> nomesOrdenados;
+
+        // Criando uma lista de pares = (categoria, nome) para facilitar a ordenação
+        std::list<std::pair<std::string, std::string>> paresCategNome;
+
+        for (const auto& projeto : Projetos) {
+            
+            paresCategNome.push_back(std::make_pair(projeto.CategoriaProjeto(), projeto.NomeProjeto()));
+        }
+
+        // Ordenando a lista de pares
+        paresCategNome.sort();
+
+        for (const auto& par : paresCategNome) {
+            nomesOrdenados.push_back(par.second);
+        }
+
+        return nomesOrdenados;
+    }
+
+
+    // Atributos da Classe
+    std::string nome_;
 
 };
 
